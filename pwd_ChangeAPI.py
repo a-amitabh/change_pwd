a8e2106788663025e0ccb50621ffaa2aca109c83
aamitabh <ashwini.amitabh@gmail.com>
2021-10-17 23:07:34 +0530

    Added unit tests to validate all password conditions

---
+++ pwd_ChangeAPI.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+import changePassword
+
+app = Flask(__name__)
+
+@app.route("/chg_pwd/<string:curr_pwd>/<string:new_pwd>", methods=['GET'])
+def chg_pwd(curr_pwd, new_pwd):
+    print(type(changePassword.changePassword(curr_pwd, new_pwd)))
+    return str(changePassword.changePassword(curr_pwd, new_pwd))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
